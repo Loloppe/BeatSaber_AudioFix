a8958f6250784a6e5e08547c17033cdcfa9f7656
Loloppe <loloppe2020@gmail.com>
2025-01-10 18:18:52 -0500

    Add project files.

---
+++ AudioFix/HarmonyPatches/Patches.cs
@@ -0,0 +1,23 @@
+﻿/*using HarmonyLib;
+using UnityEngine;
+using static AudioTimeSyncController;
+
+namespace AudioFix.HarmonyPatches
+{
+    [HarmonyPatch(typeof(AudioTimeSyncController), nameof(AudioTimeSyncController.Update))]
+    static class Audio
+    {
+        static void Prefix(AudioTimeSyncController __instance)
+        {
+            int timeSamples = __instance._audioSource.timeSamples;
+            float time = __instance._audioSource.time;
+            float num2 = __instance.timeSinceStart - __instance._audioStartTimeOffsetSinceStart;
+            float num3 = Mathf.Abs(num2 - time);
+            if ((num3 > __instance._forcedSyncDeltaTime || __instance._state == State.Paused))
+            {
+                Plugin.Log.Warn("Sync issue: " + num3.ToString() + " > " + __instance._forcedSyncDeltaTime.ToString());
+            }
+        }
+    }
+}
+*/
---
+++ AudioFix/Plugin.cs
@@ -0,0 +1,51 @@
+﻿// using HarmonyLib;
+using IPA;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace AudioFix
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        // internal static Harmony harmony;
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            BeatSaberMarkupLanguage.Util.MainMenuAwaiter.MainMenuInitializing += MainMenuInit;
+            // harmony = new Harmony("Loloppe.BeatSaber.AudioFix");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            // harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void MainMenuInit()
+        {
+            var configuration = AudioSettings.GetConfiguration();
+            configuration.dspBufferSize = 256;
+            if (AudioSettings.Reset(configuration))
+            {
+                Log.Info("dspBufferSize set to " + configuration.dspBufferSize.ToString());
+            }
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            // harmony.UnpatchSelf();
+        }
+    }
+}
